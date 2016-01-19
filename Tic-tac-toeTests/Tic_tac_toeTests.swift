187d8697b8989e704ea2bbe7b1521a6d69207963
aaronpankratz <pankratz.aaron@gmail.com>
2016-01-18 20:49:38 -0600

    Initial Commit

---
+++ Tic-tac-toeTests/Tic_tac_toeTests.swift
@@ -0,0 +1,36 @@
+//
+//  Tic_tac_toeTests.swift
+//  Tic-tac-toeTests
+//
+//  Created by Aaron Pankratz on 1/18/16.
+//  Copyright © 2016 Aaron Pankratz. All rights reserved.
+//
+
+import XCTest
+@testable import Tic_tac_toe
+
+class Tic_tac_toeTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
